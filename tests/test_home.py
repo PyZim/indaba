b6c683a58e478aa70cfaa4e916a92458fa8ba4de
hamub <hbutau35@gmail.com>
2017-03-22 23:21:53 +0200

    modified templates

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,7 +1,9 @@
 import pytest
+from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
 
-def test_home_page(rf):
-    request = rf.get("/")
-    response = TemplateView.as_view()(request)
-    assert response.template_name == "home.html"
+def test_page(rf):
+    request = rf.get(reverse("home"))
+    response = TemplateView.as_view(template_name="home.html")(request)
+    assert response.template_name[0] == "home.html"
 
